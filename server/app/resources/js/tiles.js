4bf31b0fd12fbad03292d27f39244c44e62d4267
Tobias Hilgers <th128@hdm-stuttgart.de>
2021-06-26 17:25:38 +0200

    back-up push

---
+++ server/app/resources/js/tiles.js
@@ -0,0 +1,13 @@
+"use strict"
+
+$(function(){
+    $(".tiles").mousedown(function(){      
+        $(this).addClass("selectedtile");
+    });
+  
+    $(".tiles").mouseup(function(){    
+        $(this).removeClass("selectedtile");
+    });
+});
+
+
